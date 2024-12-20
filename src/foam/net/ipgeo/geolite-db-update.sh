71cf0e90e1e78eb66fef053daf81820b4df6935c
Joel Hughes <joel@nanopay.net>
2024-12-20 10:42:54 -0500

    update download shell script to make use of /tmp

--- src/foam/net/ipgeo/geolite-db-update.sh
+++ src/foam/net/ipgeo/geolite-db-update.sh
@@ -8,27 +8,30 @@ fi
 ACCOUNT_ID=$1
 LICENSE_KEY=$2
 SAVE_TO=$3
-
-FILE_LOCATION="maxmind-update"
+DOWNLOAD_DIR="/tmp/maxmind"
+FILE_LOCATION="/tmp/maxmind/maxmind-update.tgz"
+URL="https://download.maxmind.com/app/geoip_download?edition_id=GeoLite2-City"
 
 mkdir $SAVE_TO
+mkdir $DOWNLOAD_DIR
 
-cd $SAVE_TO && {
-    curl -L -o $FILE_LOCATION "https://download.maxmind.com/app/geoip_download?edition_id=GeoLite2-City&license_key=$LICENSE_KEY&suffix=tar.gz&account_id=$ACCOUNT_ID" ;
-    if [ $? -eq 0 ]; then
-        echo "update downloaded: $FILE_LOCATION"
-    else
-        echo "updated failed"
+cd $DOWNLOAD_DIR && {
+    echo "INFO :: downloading ${URL} to ${FILE_LOCATION}"
+    curl -L -o $FILE_LOCATION $URL"&license_key=$LICENSE_KEY&suffix=tar.gz&account_id=$ACCOUNT_ID" ;
+    if [ ! $? -eq 0 ]; then
+        echo "ERROR: download failed"
         exit 1
     fi
+
+    echo "INFO :: extracting ${FILE_LOCATION} to ${SAVE_TO}/geolite"
     tar -xzf $FILE_LOCATION
     rm $FILE_LOCATION
     # rename extracted folder to a known name
     # move contents of maxmind/geolite maxmind/ and delete geolite
-    mv ./* ./geolite 
+    mv ./* "${SAVE_TO}/geolite"
     mv ./*/* .
     rm -rf geolite
     cd -;
 }
 
-echo "done"
+echo "INFO :: complete"
